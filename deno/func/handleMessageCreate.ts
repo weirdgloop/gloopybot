e045cebbf3e9130397490727acf737c319ef0441
invalidCards <842080+invalidCards@users.noreply.github.com>
2023-10-01 19:27:17 +0200

    Circumvent permission bug on members

--- deno/func/handleMessageCreate.ts
+++ deno/func/handleMessageCreate.ts
@@ -9,28 +9,28 @@ export const handleMessageCreate = async (bot: ExtendedClient, message: Harmony.
     }
 
     if (message.content.startsWith(bot.prefix)) {
-        handleCommand(bot, message, db);
+        await handleCommand(bot, message, db);
         return;
     }
 
     await LinkHandler.makeLinks(bot, message, db);
 }
 
-const messageAuthorIsAdmin = (bot: ExtendedClient, message: Harmony.Message) => {
+const messageAuthorIsAdmin = async (bot: ExtendedClient, message: Harmony.Message) => {
     if (message.author.id === bot.owner) return true;
-    if (message.member?.permissions.has(Harmony.PermissionFlags.ADMINISTRATOR)) return true;
+    if (await memberHasRoleWithAdmin(message.member)) return true;
     if (message.channel.isDM() || message.channel.isGroupDM()) return true;
     return false;
 }
 
-const handleCommand = (bot: ExtendedClient, message: Harmony.Message, db: SQLite.DB) => {
+const handleCommand = async (bot: ExtendedClient, message: Harmony.Message, db: SQLite.DB) => {
     const [cmd, ...args] = message.content.replace(bot.prefix, '').split(' ');
     if (!commands[cmd]) {
         message.reply(`Command "${cmd}" not found!`);
         return;
     }
 
-    if (commands[cmd].restricted && !messageAuthorIsAdmin(bot, message)) {
+    if (commands[cmd].restricted && !(await messageAuthorIsAdmin(bot, message))) {
         if (commands[cmd].excludeFromHelp) message.reply(`Command "${cmd}" not found!`);
         else message.reply(`You are not permitted to run the "${cmd}" command.`);
         return;
@@ -49,4 +49,19 @@ const botHasSendMessagePerms = async (message: Harmony.Message) => {
     }
 
     return false;
+}
+
+const memberHasRoleWithAdmin = async (member?: Harmony.Member) => {
+    if (!member) return false;
+
+    let found = false;
+    const roles = await member.roles.array();
+
+    for (const role of roles) {
+        if (role.permissions.has(Harmony.PermissionFlags.ADMINISTRATOR)) {
+            found = true;
+        }
+    }
+
+    return found;
 }
