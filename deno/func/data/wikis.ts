bc8c7302d71df1f6b840c8da01feea7f1eb6720c
MiladyCheese <207770915+MiladyCheese@users.noreply.github.com>
2025-04-15 16:32:28 -0400

    add missing metawg

--- deno/func/data/wikis.ts
+++ deno/func/data/wikis.ts
@@ -40,7 +40,7 @@ export const wikis : Record<string, WikiDefinition> = {
     g: {
         url: "https://meta.weirdgloop.org",
         name: "RuneScape Wiki Meta",
-        aliases: ["g", "gloop", "weirdgloop"]
+        aliases: ["metawg", "g", "gloop", "weirdgloop"]
     },
     b: {
         url: "https://brightershoreswiki.org",
